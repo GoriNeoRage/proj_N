f2b6daf4959bb5e7bc95c80909853c853c12c95a
Alina <lina.sushylova@gmail.com>
2023-07-19 14:45:33 +0200

    added new rules

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const numberOfFilms = +prompt ("How many films have you already seen?", " ")
 
 const personalMovieDB = {
@@ -7,14 +9,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt ("What was the last film you have seen?", " "),
-      b = +prompt ("Rate the film", " "),
-      c = prompt ("What was the last film you have seen?", " "),
-      d = +prompt ("Rate the film", " ")
 
 
+for ( let i = 0; i < 2; i++){
+      const a = prompt ("What was the last film you have seen?", " "),
+            b = +prompt ("Rate the film", " ");
+    if (a != null && b != null && a != "" && b!= "" && a.length <50){
+        personalMovieDB.movies[a]=b;
+        console.log("done");
+    } else{
+        console.log("error");
+        i--
+    }
+}     
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+if(personalMovieDB.count < 10){
+    console.log("There are not many watched movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are the perfect client");
+} else if (personalMovieDB.count >= 30){
+    console.log("You are film adicted person");
+} else {
+    console.log("There is an error");
+}
 
 console.log(personalMovieDB);
